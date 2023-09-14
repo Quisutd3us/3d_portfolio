9991d8c66effb4a6f983aec624c3be8f69654c9a
d3us <dnarino@gmail.com>
2023-09-14 13:10:36 -0500

    develop skeleton of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 // import Style 
 import './App.css'
 
@@ -6,8 +7,20 @@ const App = () => {
 
   return (
     <>
-      <div>
-        3d Developer Portfolio
+      <div className='relative z-o bg-primary'>
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact/>
+          <StarsCanvas/>
+        </div>
       </div>
     </>
   )
