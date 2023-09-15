0c3672da403b75ae1175f35bb741087c85a6d59f
d3us <dnarino@gmail.com>
2023-09-15 11:30:46 -0500

    import GLTF model and confire lights canvas and cameras

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,24 +5,29 @@ import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <section className={`${styles.helperBlue} relative w-full h-screen mx-auto`}>
-      <div className={`${styles.paddingX} ${styles.helperWhite} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-        {/* down line decoration */}
-        <div className={`${styles.helperRed} flex flex-col justify-center items-center mt-5`}>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+      <section className={`relative w-full h-screen mx-auto`}>
+        <div
+            className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        >
+          <div className='flex flex-col justify-center items-center mt-5'>
+            <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+            <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          </div>
+
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>
+              Hi, Im <span className='text-[#915EFF]'>Adrian</span>
+            </h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              I develop 3D visuals, user <br className='sm:block hidden' />
+              interfaces and web applications
+            </p>
+          </div>
         </div>
-        {/* /down line decoration */}
-        {/* text hero */}
-        <div className={`${styles.helperBlue} text-white`}>
-          <h1 className={`${styles.heroHeadText}`}>Hi , I'm <span className='text-[#915eff]'>David Nariño</span> </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>Im a FrontEnd , 3d Visuals and <br className={' hidden sm:block '}/> middle MEAN stack Developer </p>
-        </div>
-        {/* /text hero */}
-      </div>
-        <ComputersCanvas/>
+
+        <ComputersCanvas />
     </section>
   )
 }
 
-export default Hero
+export default Hero;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,22 +6,31 @@ import CanvasLoader from '../Loader';
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
-
   return (
       <mesh>
-        <hemisphereLight intensity={0.15} groundColor={'black'}/>
+        <hemisphereLight intensity={0.75} groundColor='black'/>
         <pointLight intensity={1}/>
+        <spotLight
+            position={[-20, 50, 10]}
+            angle={0.12}
+            penumbra={1}
+            intensity={1}
+            castShadow
+        />
         <primitive
             object={computer.scene}
+            scale={0.75}
+            position={[0, -3.25, -1.5]}
+            rotation={[0.01, -0.2, -0.1]}
         />
       </mesh>
-  )
-}
+  );
+};
 
-const ComputerCanvas = () => {
+const ComputersCanvas = () => {
   return (
       <Canvas
-          frameLoop="demand"
+          frameloop='demand'
           shadows
           camera={{position: [20, 3, 5], fov: 25}}
           gl={{preserveDrawingBuffer: true}}
@@ -34,10 +43,10 @@ const ComputerCanvas = () => {
           />
           <Computers/>
         </Suspense>
-        <Preload/>
+        <Preload all/>
       </Canvas>
 
   );
 };
 
-export default Computers
+export default ComputersCanvas;
