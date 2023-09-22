e37691edc1e4011e74fc102026653c652ef17ee8
d3us <dnarino@gmail.com>
2023-09-22 15:11:56 -0500

    setting email sender funcion with emailjs library

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,6 +8,8 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
+
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -17,8 +19,47 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => { };
-  const handleSubmit = () => { };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value
+    })
+  };
+
+  // template_esqqw2j
+  // service_qssk9cq
+  // duUNIn9XYl4b1MAq7
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs.send(
+      'service_qssk9cq',
+      'template_esqqw2j',
+      {
+        from_name: form.name,
+        to_name: 'David',
+        from_email: form.email,
+        to_email: 'dnarino@gmail.com',
+        message: form.message
+      },
+      'duUNIn9XYl4b1MAq7'
+    )
+    .then(()=>{
+      setLoading(false);
+      alert('Thank you , I will get back to you as soon as possible');
+      setForm({
+        name:'',
+        email:'',
+        message:''
+      });
+    },(error)=>{
+      setLoading(false);
+      console.log(error);
+      alert('Something went wrong...')
+    })
+
+  };
 
 
   return (
@@ -55,7 +96,7 @@ const Contact = () => {
             <input
               type={'email'}
               name={'email'}
-              value={form.message}
+              value={form.email}
               onChange={handleChange}
               placeholder={"What's your Email ?"}
               className={"bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"}
@@ -68,26 +109,26 @@ const Contact = () => {
             <textarea
               rows={7}
               name={'message'}
-              value={form.email}
+              value={form.message}
               onChange={handleChange}
               placeholder={"What do you want to say ?"}
               className={"bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"}
             />
           </label>
           <button
-          type='submit'
-          className={"bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"}
+            type='submit'
+            className={"bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"}
           >
-            {loading? 'Sending ...' : 'Send'}
+            {loading ? 'Sending ...' : 'Send'}
           </button>
         </form>
       </motion.div>
       <motion.div
         variants={slideIn('right', 'tween', 0.2, 1)}
-        className={`xl:flex-1 xl:h-auto md:h-[550px] h-[350px]` }
+        className={`xl:flex-1 xl:h-auto md:h-[550px] h-[350px]`}
       >
         {/* import 3d object model  */}
-        <EarthCanvas/>
+        <EarthCanvas />
       </motion.div>
     </div>
   )
